52fc3bef2b7ec906ca45f1aa4c89d066183a8065
Arbaz Kaladiya <kaladiya.arbaz@gmail.com>
2025-11-21 18:57:45 +0530

    initail Setup

---
+++ CricbuzzMachineRound/Common/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  CricbuzzMachineRound
+//
+//  Created by Arbaz Kaladiya on 21/11/25.
+//
+
+import SwiftUI
+
+struct Constants: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Constants()
+}
